8693cf925d08f488506b6afa67a79b0396133818
Monalisa-S-C <monalisa.silva06@aluno.ifce.edu.br>
2024-12-15 20:42:58 +0000

    vinculação dos templates

--- ProjetoDjango/paginaDinamica_app/urls.py
+++ ProjetoDjango/paginaDinamica_app/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('home', views.home, name='home'),
     path('index/', views.index, name='index'),
     path('resultado/', views.resultado, name='resultado'),
     path('autor/', views.autor, name='autor'),
